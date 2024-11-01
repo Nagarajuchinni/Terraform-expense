b0dcceb019372627b62cae0fd8f5c3d8c9721e21
Nagarajuchinni <nagarajchinni3@gmail.com>
2024-11-01 17:55:40 +0530

    test

--- main.tf
+++ main.tf
@@ -1,54 +1,54 @@
-resource "aws_instance" "Frontend" {
+resource "aws_instance" "instances" {
+    for_each = var.components
     ami = var.ami
-    instance_type = var.instance_type
+    instance_type = each.value["instance_type"]
     vpc_security_group_ids = var.sg_id
     tags = {
-      Name = "Frontend"
+      Name = "${each.value["Name"]}-${var.env}"
     }
   
 }
 
-resource "aws_route53_record" "Frontend" {
-  zone_id = var.zone_id
-  name    = "Frontend-${var.env}"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.Frontend.private_ip]
-}
+# resource "aws_route53_record" "Frontend" {
+#   zone_id = var.zone_id
+#   name    = "Frontend-${var.env}"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.Frontend.private_ip]
+# }
 
-resource "aws_instance" "Backend" {
-    ami = var.ami
-    instance_type = var.instance_type
-    vpc_security_group_ids = var.sg_id
-    tags = {
-      Name = "Backend"
-    }
+# resource "aws_instance" "Backend" {
+#     ami = var.ami
+#     instance_type = var.instance_type
+#     vpc_security_group_ids = var.sg_id
+#     tags = {
+#       Name = "Backend"
+#     }
   
-}
+# }
 
-resource "aws_route53_record" "Backend" {
-  zone_id = var.zone_id
-  name    = "Backend-${var.env}"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.Backend.private_ip]
-}
+# resource "aws_route53_record" "Backend" {
+#   zone_id = var.zone_id
+#   name    = "Backend-${var.env}"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.Backend.private_ip]
+# }
 
-resource "aws_instance" "mysql" {
-    ami = var.ami
-    instance_type = var.instance_type
-    vpc_security_group_ids = var.sg_id
-    tags = {
-      Name = "mysql"
-    }
+# resource "aws_instance" "mysql" {
+#     ami = var.ami
+#     instance_type = var.instance_type
+#     vpc_security_group_ids = var.sg_id
+#     tags = {
+#       Name = "mysql"
+#     }
   
-}
+# }
 
-resource "aws_route53_record" "mysql" {
-  zone_id = var.zone_id
-  name    = "Mysql-${var.env}"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mysql.private_ip]
-}
-
+# resource "aws_route53_record" "mysql" {
+#   zone_id = var.zone_id
+#   name    = "Mysql-${var.env}"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.mysql.private_ip]
+# }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,19 @@
 env = "dev"
 ami = "ami-0b4f379183e5706b9"
-instance_type = "t2.micro"
 sg_id = ["sg-05c6fc961188560fd"]
 zone_id = "Z08026271RHGWGFS6JD44"
+
+components = {
+    frontend = {
+        name = "frontend"
+        instance_type = "t2.micro"
+    }
+    backend = {
+        name = "backend"
+        instance_type = "t2.micro"
+    }
+    mysql = {
+        name = "mysql"
+        instance_type = "t2.micro"
+    }
+}
